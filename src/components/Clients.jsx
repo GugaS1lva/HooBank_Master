e05ad223c7e84822494cfa9e489c2c1b5c196d84
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-10-06 17:31:05 -0300

    Finished Clients [✓]

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map(client => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[100px] hover:brightness-200`}>
+            <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain' />
+          </div>
+        ))}
+      </div>
+
+    </section>
   )
 }
 
